1f7fdf021dcf409bf9a7a2600f0b48e418f73019
Hrishikesh <hrishikeshanand3@gmail.com>
2024-10-26 11:37:59 +0530

    Accordian - FirstProject

---
+++ accordion/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Accordian from './Components/Accordian';
+
+function App() {
+  return (
+    <div className="App">
+      <Accordian></Accordian>
+    </div>
+  );
+}
+
+export default App;
