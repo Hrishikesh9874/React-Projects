d2ca351b29ac851bb062064984a4d16656717ded
Hrishikesh <hrishikeshanand3@gmail.com>
2024-10-26 23:09:15 +0530

    Committing Project - Random-Color

---
+++ random-color/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Random from './Random.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Random></Random>
+    </div>
+  );
+}
+
+export default App;
