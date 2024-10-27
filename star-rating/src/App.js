12a830ecb6c41623a102ad2348caa74680685240
Hrishikesh <hrishikeshanand3@gmail.com>
2024-10-27 14:57:00 +0530

    Commmitting New Project - STAR RATING

---
+++ star-rating/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StarRating from './StarRating';
+
+function App() {
+  return (
+    <div className="App">
+      <StarRating noOfStars={10}></StarRating>
+    </div>
+  );
+}
+
+export default App;
