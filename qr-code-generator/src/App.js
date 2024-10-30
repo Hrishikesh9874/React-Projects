1321b0e9949bf6aa3db23e5fc106138e48565854
Hrishikesh <hrishikeshanand3@gmail.com>
2024-10-31 00:31:46 +0530

    qr-generator

---
+++ qr-code-generator/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import QrGenerator from './QrGenerator'; 
+
+function App() {
+  return (
+    <div className='App'>
+      <QrGenerator/>
+    </div>
+  );
+}
+
+export default App;
