cbcc52156e582111044d24a4af37cbe4de4634a3
Hrishikesh <hrishikeshanand3@gmail.com>
2024-11-03 22:36:45 +0530

    Dynamic Tab Project

---
+++ custom-tab/src/App.js
@@ -0,0 +1,11 @@
+import TabTest from "./tab-test";
+
+function App() {
+  return (
+    <div>
+      <TabTest></TabTest>
+    </div>
+  );
+}
+
+export default App;
---
+++ custom-tab/src/tab-test.js
@@ -0,0 +1,36 @@
+import Tabs from "./Tabs"
+
+
+function RandomComponent(){
+    return(
+        <h1>Some Random Content</h1>
+    )
+}
+
+export default function TabTest(){
+
+    const tabs= [
+        {
+            label: 'Tab 1',
+            content: <div>This is content for Tab 1</div>
+        },
+        {
+            label: 'Tab 2',
+            content: <div>This is content for Tab 2</div>
+        },
+        {
+            label: 'Tab 3',
+            content: <RandomComponent/>
+        }
+    ];
+
+    function handleChange(currentTabIndex){
+        
+    }
+
+    return(
+        <>
+            <Tabs tabsContent={tabs} onChange={handleChange}></Tabs>
+        </>
+    )
+}
