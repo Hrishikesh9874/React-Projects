2bfeae7fed3e2d480b57d90c742fe8ae72823db0
Hrishikesh <hrishikeshanand3@gmail.com>
2024-11-16 22:22:06 +0530

    No mssz

---
+++ use-outside-click/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UseOnClickOutsideTest from './UseOnClickOutsideTest';
+
+function App() {
+  return (
+    <div className="App">
+      <UseOnClickOutsideTest></UseOnClickOutsideTest>
+    </div>
+  );
+}
+
+export default App;
