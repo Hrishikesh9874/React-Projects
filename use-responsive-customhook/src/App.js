eccf4bceea93ef143b3ac241104b0e85eb3cdf9a
Hrishikesh <hrishikeshanand3@gmail.com>
2024-11-17 22:40:28 +0530

    No Message

---
+++ use-responsive-customhook/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UseWindowResizeTest from './UseWindowResizeTest';
+
+function App() {
+  return (
+    <div className="App">
+      <UseWindowResizeTest></UseWindowResizeTest>
+    </div>
+  );
+}
+
+export default App;
